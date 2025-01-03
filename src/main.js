1678dac0415f3fe605c985791240177c198528d1
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-01-03 07:49:19 +0600

    bg img set every zoom effect content

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
+// import gsap from "gsap";
+import gsap from "gsap";
 const slider = document.querySelector(".slider");
 const sliderItem = slider.querySelectorAll(".item");
 (function () {
   const sliderContentItems = slider.querySelectorAll(".slider_zoom_content");
+  sliderContentItems.forEach((el) => {
+    const bgImg = el.getAttribute("data-bg-src");
+    gsap.set(el, {
+      backgroundImage: `url('${bgImg}')`,
+      duration: 1.5,
+      backgroundSize: "150%",
+      backgroundOrigin: "padding-box",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      ease: "none",
+    });
+    if (!bgImg) {
+      gsap.set(el, {
+        backgroundColor: "gray",
+        duration: 1.5,
+        backgroundSize: "200%",
+        ease: "expo.in",
+      });
+    }
+  });
+  // here start carousel
 })();
