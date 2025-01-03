473724c68307933d812672376fd102541cdfd80c
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-01-03 08:26:37 +0600

    set font here

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playFire: ['"Playfair Display"', "serif"],
+        unbound: ['"Unbounded"', "serif"],
+      },
+    },
   },
   plugins: [],
 };
